e2314cdbf56b76d0529fe8093b84e41a2332ff32
borbalagera <borbalagera@gmail.com>
2020-06-26 18:14:29 +0200

    Updates

---
+++ precache-manifest.347f343ea5fb8483831fb2ae017dd466.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcedc5dd27e046b7cc4f3b70f913b717",
+    "url": "/github-quotes/index.html"
+  },
+  {
+    "revision": "bb1d0fe21a6c418bff9a",
+    "url": "/github-quotes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ba8d4585b293f82cb234",
+    "url": "/github-quotes/static/js/2.975fa792.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-quotes/static/js/2.975fa792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb1d0fe21a6c418bff9a",
+    "url": "/github-quotes/static/js/main.53884aad.chunk.js"
+  },
+  {
+    "revision": "9811a2cea86bde8a6741",
+    "url": "/github-quotes/static/js/runtime-main.45997c97.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-quotes/static/media/logo.5d5d9eef.svg"
+  }
+]);
