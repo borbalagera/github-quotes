ae405853e4f670843801fedb2f1e1dd039b3f90e
borbalagera <borbalagera@gmail.com>
2020-06-26 21:41:48 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github-quotes/precache-manifest.373d7ee409619edfb2add76aa7fa1aa5.js"
+  "/github-quotes/precache-manifest.a3330f204ccf2d079451609e9a80fd57.js"
 );
 
 self.addEventListener('message', (event) => {
